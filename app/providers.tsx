5ff604adbdf8c9ed850f74df4dbaf51c9de8ddd7
Kağan Hamzaçebi <kagan.hcebi@gmail.com>
2024-12-29 16:40:16 +0300

    react-query-client change

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,7 +7,7 @@ import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import { User } from '@supabase/supabase-js';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createContext, ReactNode, Suspense, useContext, useState } from 'react';
+import { createContext, ReactNode, Suspense, useContext } from 'react';
 
 const UserContext = createContext<User | null>(null);
 
@@ -20,9 +20,15 @@ interface IProviders {
   children: ReactNode;
 }
 
-export default function Providers({ children, user }: Readonly<IProviders>) {
-  const [queryClient] = useState(() => new QueryClient());
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000 // 1 hour
+    }
+  }
+});
 
+export default function Providers({ children, user }: Readonly<IProviders>) {
   return (
     <UserContext.Provider value={user}>
       <QueryClientProvider client={queryClient}>
